f76c0de4d2e40e62fdd513c84b799aafdef8cea0
Udo Schroeter <udo.schroeter@gmail.com>
2014-01-09 05:30:42 +0100

    beware the smooth navigating wizards

--- js/map.js
+++ js/map.js
@@ -21,5 +21,26 @@ var gameMap = {
   update : function() {
     
     },
+    
+  makeCell : function(cellInfo) {
+    
+    mapState.container.append('<div class="mapblock" style="left:'+(cellInfo.pos.x*config.tileSize)+'px;top:'+(cellInfo.pos.y*config.tileSize)+'px;">X</div>');
+    mapState.pfGrid.setWalkableAt(cellInfo.pos.x, cellInfo.pos.y, false);
+    
+    },
+
+  loadTextMap : function(textLines) {
+    $.each(textLines, function(y, line) {
+      
+      for(var x = 0; x < line.length; x++) {
+        
+        if(line.charAt(x) == 'X') {
+          gameMap.makeCell({ pos : { x : x, y : y } });
+        }
+        
+      }
+      
+      });    
+    },
   
   };
