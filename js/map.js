2815d48b6f64c96cd738dc109a64a02e0e855202
Udo Schroeter <udo.schroeter@gmail.com>
2014-01-09 03:04:10 +0100

    basic map element

---
+++ js/map.js
@@ -0,0 +1,19 @@
+var mapState = {};
+
+var gameMap = {
+  
+  setDisplayContainer : function(e) {
+    mapState.container = e;
+    return(this);
+    },
+    
+  init : function() {
+    return(this);
+    },
+    
+  display : function() {
+
+    return(this);
+    }  
+  
+  };
